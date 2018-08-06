072382957504260eaac12d05572446f16f185ead
Startup <Startup@KTLSB3PRO26s-MacBook-Pro.local>
2018-08-06 23:14:14 +0530

    made gps_read file

---
+++ gps_read.py
@@ -0,0 +1,22 @@
+import serial
+import pynmea2
+
+port = "/dev/ttyAMA0"
+
+ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
+
+while 1:
+    try:
+        data = ser.readline()
+    except:
+        print("loading")
+
+    if data[0:6] == '$GPGGA':
+        msg = pynmea2.parse(data)
+        latval = msg.lat
+        concatlat = "lat:" + str(latval)
+        print(concatlat)
+        longval = msg.lon
+        concatlong = "long:" + str(longval)
+        print (concatlong)
+        
